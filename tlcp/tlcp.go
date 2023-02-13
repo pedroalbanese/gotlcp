0b67920c9ffeffdedbfd5c7af6c52c27bc75b3fe
Pedro F. Albanese <68971450+pedroalbanese@users.noreply.github.com>
2023-02-13 11:54:35 -0300

    Update tlcp.go

--- tlcp/tlcp.go
+++ tlcp/tlcp.go
@@ -281,16 +281,21 @@ func parsePrivateKey(der []byte) (crypto.PrivateKey, error) {
 	//if key, err := x509.ParsePKCS1PrivateKey(der); err == nil {
 	//	return key, nil
 	//}
-	/*
+	
 	if key, err := x509.ParsePKCS8PrivateKey(der); err == nil {
 		switch key := key.(type) {
-		case *rsa.PrivateKey, *ecdsa.PrivateKey, *sm2.PrivateKey:
+//		case *rsa.PrivateKey, *ecdsa.PrivateKey, *sm2.PrivateKey:
+		case *rsa.PrivateKey:
 			return key, nil
 		default:
 			return nil, errors.New("tlcp: found unknown private key type in PKCS#8 wrapping")
 		}
 	}
-	*/
+	
+	if key, err := x509.ParseECPrivateKey(der); err == nil {
+		return key, nil
+	}
+
 	if key, err := x509.ParseSM2PrivateKey(der); err == nil {
 		return key, nil
 	}
